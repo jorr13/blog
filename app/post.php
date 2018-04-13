df9cb32a9918c3c70b8b9839d5d35f91963504af
Andrés Salazar <salazarcode@gmail.com>
2018-04-13 15:31:00 -0400

    Guía de estilo y sintaxis

--- app/post.php
+++ app/post.php
@@ -6,11 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class post extends Model
 {
-    protected $table = 'posts';
+    /*
+    * No le tienes que poner el nombre de la tabla 
+    * a menos que el nombre de la tabla
+    * no sea el plural exacto del
+    * nombre del modelo
+    */
+    //protected $table = 'posts';
 
-    protected $fillable = ['titulo', 'contenido'];
-
-    public $timestamps = true;
-
-
+    protected $fillable = ['titulo', 'contenido'];    
+    
+    /*
+    * Por defecto los modelos mantienen 
+    * los timestamp por su cuenta, cuando 
+    * creas un nuevo bjeto de esta clase 
+    * en la base de datos, ELOQUENT pone 
+    * la fecha, así que ésta linea sobra
+    */
+    //public $timestamps = true;
 }
